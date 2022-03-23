1b1dd549658aae80836b927cf83052ccebec2280
hotshotxay <94151865+hotshotxay@users.noreply.github.com>
2022-03-23 17:38:23 -0400

    Final Product of Website

---
+++ script.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("booper");
+
+function addEmoji () {
+    const emojis = [
+        '🐕','🐕‍🦺','😀','😊','🐩','😉','😍','✨','❤️','👍','🎉','🔥','😝','😛',
+        ];
+    const emojispot = document.getElementById("emojispot")
+    emojispot.append(emojis[Math.floor(Math.random() * emojis.length)])
+}
+
+button.addEventListener("click", addEmoji, false);
